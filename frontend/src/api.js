49cb962e5d768b9cd0a609e3f74b4be5e325dca5
Neeraj Rawat <nsrawat0333@gmail.com>
2026-08-23 02:35:44 +0530

    Increase API timeout to 45 seconds so sleeping Render backend has plenty of time to wake up and save bookings

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -10,7 +10,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000
+  timeout: 45000
 });
 
 export const createBooking = async (bookingData) => {
